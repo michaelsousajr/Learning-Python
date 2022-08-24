6bf99b860224a1905bc67bad9187a59f0dd2698d
Michael <89663481+michaelsousajr@users.noreply.github.com>
2022-08-24 13:56:03 -0400

    Add files via upload

---
+++ learnpy.py
@@ -0,0 +1,19 @@
+#Learning Python!
+
+def smileyFace():
+    print("[]   []")
+    print("  /_   ")
+    print("       ")
+    print("|_____|")
+
+def datatypevar():
+    charAge = "21" #this is a string
+    charName = "Michael"
+    print("My name is " + charName +" and I am " + charAge + " years old.")   
+
+def strings():
+    cat = "charlie"
+    print(cat + " is cool")
+    print(cat.upper())
+    
+    
