02dc0d938483c289692d448d06d23398dde6d565
Michael <michaelsousajr@gmail.com>
2022-08-24 14:15:28 -0400

    Update learnpy.py

--- learnpy.py
+++ learnpy.py
@@ -15,5 +15,9 @@ def strings():
     cat = "charlie"
     print(cat + " is cool")
     print(cat.upper())
-    
-    
+    print(cat.upper().isupper()) #true or false if string is uppercase
+    print(len(cat)) #length of str
+    print(cat[0]) #prints first index
+    print(cat.index("a")) #letter a is at index 2
+    print(cat.replace("lie","les")) #replaces specified letters
+        
