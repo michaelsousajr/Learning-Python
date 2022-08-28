c48d7542772163f1c4b1a4ad22a570cc9edf19d8
Michael <michaelsousajr@gmail.com>
2022-08-28 11:30:50 -0400

    Update learnpy.py

--- learnpy.py
+++ learnpy.py
@@ -36,3 +36,31 @@ def numbers():
 def myInput():
     name = input("Enter name: ")
     print("Length of your name is: " + len(name))
+
+def calculate():
+    num1 = input("Enter number: ") #input is always read as str 
+    num2 = input("Enter second number: ")
+    result = int(num1) + int(num2) #int turns input to int
+    print(result)
+    
+def myList():
+    friends = ["bob","jim","kim"]
+    print(friends[0]) #prints first element in list
+
+def listfnx():
+    luckyNums = [7,11,13,0,5]
+    friends = ["bob","jim","kim"]
+    friends.extend(luckyNums) #adds luckynums to back of list
+    friends.append("Mike") #adds 1 element to list
+    friends.insert(1, "kelly") #inserts at index 
+    friends.remove("jim") #jim is removed
+    friends.pop() #remove last element from list
+    print(friends.index("bob")) #prints index of element
+    print(friends.count("bob")) #counts how many bobs in list
+    luckyNums.sort() #sorts luckynum list
+    friends2 = friends.copy() #copies list
+    print(friends)
+
+def tuples():
+    coordinates = (3, 5)
+    print(coordinates[0])
