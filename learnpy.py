c800dc6d105586d8ce861693f5b6a694802ee81c
Michael <michaelsousajr@gmail.com>
2022-09-08 11:26:07 -0400

    Update learnpy.py

--- learnpy.py
+++ learnpy.py
@@ -208,5 +208,12 @@ def comment():
     
     print("this line works")
     
-    
-    
+def tryExcept():
+    try:
+        answer = 10/0
+        number = int(input("Enter a number: "))
+        print(number)
+    except ZeroDivisionError as err: 
+        print(err)
+    except ValueError:
+        print("invalid input")
